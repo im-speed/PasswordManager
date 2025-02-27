1ef5510d4384bb57ecb2d1f60866983e8d3f48bf
im-speed <joakim.gietl@gmail.com>
2025-02-27 14:23:35 +0100

    Added code to run a command

--- src/PasswordManager/Program.cs
+++ src/PasswordManager/Program.cs
@@ -2,7 +2,27 @@
 
 class Program
 {
+    static Dictionary<string, Action<string[]>> Commands { get; } = new()
+    {
+    };
+
     static void Main(string[] args)
     {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("No command provided");
+            return;
+        }
+
+        string commandName = args[0];
+        string[] commandArgs = args.Skip(1).ToArray();
+
+        if (!Commands.TryGetValue(commandName, out Action<string[]>? command))
+        {
+            Console.WriteLine($"Unknown command: {commandName}.");
+            return;
+        }
+
+        command(commandArgs);
     }
 }
