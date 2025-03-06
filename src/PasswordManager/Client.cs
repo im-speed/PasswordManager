40765122e9e5a4794ef89f0a69fa604ba236c5e1
Joakim Gietl <joakim.gietl@gmail.com>
2025-03-06 14:57:54 +0100

    Removed unecessary udings

--- src/PasswordManager/Client.cs
+++ src/PasswordManager/Client.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using System.Text.Json;
 using PasswordManager.JsonClasses;
 using PasswordManager.Keys;
--- src/PasswordManager/Server.cs
+++ src/PasswordManager/Server.cs
@@ -1,5 +1,4 @@
 using System.Security.Cryptography;
-using System.Text;
 using System.Text.Json;
 using PasswordManager.JsonClasses;
 using PasswordManager.Keys;
